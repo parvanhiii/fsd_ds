862477fc4f743cc8f8d71c94ffc8750940f3916e
parvanhiii <parvanisood24@gmail.com>
2024-10-14 10:31:01 +0530

    js

---
+++ day5/script.js
@@ -0,0 +1,49 @@
+// function display(){
+//     console.log("hello")
+// }
+// let x=14;
+// console.log("hello js",x)
+// display();
+// let x=12.23;
+// let y="hello"
+// let z=true
+// console.log(typeof z)
+// let arr=[12,1,5,6,9,3]
+// let a;
+// let b=null;
+// console.log(typeof b)
+// function validate(){
+//     let name=document.getElementById("un").value;
+//     let pss=document.getElementById("pass").value;
+//   if(un=='admin'&& pass=='admin'){
+//   document.getElementById("res").innerHTML="login success";
+//   }
+// }
+
+// let z=Number("12");
+// let str= Number("123");
+// console.log('12'+'3');
+// let b=Boolean(0)
+// console.log(b);
+// "use strict"
+// let num=12;
+
+// let a=12;
+// let b=12;
+// console.log(a==b);
+
+// let a=(x,y)=>{
+//     console.log(x+y);
+// };
+
+// a(3,4);
+
+// let say=()=>console.log("hello")
+// say();
+
+function display(){
+    let pl=document.querySelector("p");
+    pl.innerHTML="result";
+    pl.style.backgroundColor="red";
+}
+display();
